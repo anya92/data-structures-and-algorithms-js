bfb952607f01939e300324f4497118863fad7b0c
Ania <aniabrajtkopf@gmail.com>
2018-10-03 13:57:37 +0200

    added more recursion functions

--- Recursion/recursion.js
+++ Recursion/recursion.js
@@ -30,3 +30,59 @@ function sumRange(num) {
 }
 
 console.log(sumRange(4));
+
+function factorialIteratively(num) {
+  let total = 1;
+  for (let i = num; i > 1; i--) {
+    total *= i;
+  }
+  return total;
+}
+
+console.log(factorialIteratively(3));
+
+function factorialRecursively(num) {
+  if (num <= 1) return 1;
+  return num * factorialRecursively(num - 1);
+}
+
+console.log(factorialRecursively(4));
+
+// Recursion as a helper method
+
+function collectOddValues(arr) {
+  let result = [];
+
+  function helper(helperInput) {
+    if (helperInput.length === 0) return;
+    if (helperInput[0] % 2 !== 0) result.push(helperInput[0]);
+    helper(helperInput.slice(1));
+  }
+
+  helper(arr);
+  return result;
+}
+
+console.log(collectOddValues([1, 2, 3, 4, 5]));
+
+// the same as a pure recursion
+
+function collectOddValuesRecursively(arr) {
+  let newArr = [];
+
+  if (arr.length === 0) return newArr;
+  if (arr[0] % 2 !== 0) newArr.push(arr[0]);
+
+  newArr = newArr.concat(collectOddValuesRecursively(arr.slice(1)));
+  return newArr;
+}
+
+console.log(collectOddValuesRecursively([1, 2, 3, 4, 5]));
+
+
+/*
+  pure recursion tips:
+  - for arrays use slice, the spread operator or concat to make copies of arrays so you do not mutate them,
+  - strings are immutable so you will need to use methods like slice, substr or substring
+  - for objects: Object.assign or the spread operator
+*/
