ce4b14c4649dd86db7778e538817c7f31599b752
Ania <aniabrajtkopf@gmail.com>
2018-10-12 12:52:54 +0200

    added get and set methods to SLL

--- 7.DataStructures/singlyLinkedLists.js
+++ 7.DataStructures/singlyLinkedLists.js
@@ -43,16 +43,7 @@ class SinglyLinkedList {
     return currentNode;
   }
 
-  shift() { // remove a node from the beginning of the Linked List
-    if (!this.head) return undefined;
-    const currentHead = this.head;
-    this.head = currentHead.next;
-    this.length--;
-    if (this.length === 0) this.tail = null;
-    return currentHead;
-  }
-
-  unshift(value) { // add a new node to the beginning of the Linked List
+  unshift(value) { // adding a new node to the beginning of the Linked List
     const newNode = new Node(value);
     if (!this.head) {
       this.head = newNode;
@@ -64,13 +55,43 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  shift() { // removing a node from the beginning of the Linked List
+    if (!this.head) return undefined;
+    const currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) this.tail = null;
+    return currentHead;
+  }
+
+  get(index) { // retrieving a node by its position in the Linked List
+    if (index < 0 || index >= this.length) return null;
+    let i = 0;
+    let currentNode = this.head;
+    while (i < index) {
+      currentNode = currentNode.next;
+      i++;
+    }
+    return currentNode;
+  }
+
+  set(index, value) { // changing the value of a node based on its position in the Linked List
+    let node = this.get(index);
+    if (!node) return false;
+    node.value = value;
+    return true;
+  }
 }
 
 let list = new SinglyLinkedList();
 list.push(2);
 list.push(5);
 list.push(8);
-console.log(JSON.stringify(list, null, ' '));
 list.pop();
 list.shift();
 list.unshift(0);
+list.push(14);
+console.log(JSON.stringify(list, null, ' '));
+console.log(list.get(1))
+console.log(list.set(3, 4))
