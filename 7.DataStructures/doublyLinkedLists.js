866544385fe1271afcf746107f4eae48a336dfcb
Ania <aniabrajtkopf@gmail.com>
2018-10-13 18:55:10 +0200

    added Doubly Linked List + push and pop methods

---
+++ 7.DataStructures/doublyLinkedLists.js
@@ -0,0 +1,59 @@
+/*
+  Doubly Linked List
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) { // adding a new node to the end of the Doubly Linked List
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() { // removing a node from the end of the Doubly Linked List
+    if (!this.tail) return undefined;
+    let oldTail = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = oldTail.prev;
+      this.tail.next = null;
+      oldTail.prev = null;
+    }
+    this.length--;
+    return oldTail;
+  }
+}
+
+let list = new DoublyLinkedList();
+list.push(0);
+list.push(1);
+console.log(list);
+list.pop();
+console.log(list);
+list.pop();
+console.log(list);
+
+
