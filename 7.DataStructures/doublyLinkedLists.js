fa66941205ae0fc3fe303a2c56b1695cf739a730
Ania <aniabrajtkopf@gmail.com>
2018-10-14 15:33:19 +0200

    added shift and unshift methods to the doubly linked list

--- 7.DataStructures/doublyLinkedLists.js
+++ 7.DataStructures/doublyLinkedLists.js
@@ -45,15 +45,46 @@ class DoublyLinkedList {
     this.length--;
     return oldTail;
   }
+
+  unshift(value) { // adding a new node to the beginning of the Doubly Linked List
+     const newNode = new Node(value);
+     if (!this.head) {
+       this.head = newNode;
+       this.tail = newNode;
+     } else {
+       newNode.next = this.head;
+       this.head.prev = newNode;
+       this.head = newNode;
+     }
+     this.length++;
+     return this;
+  }
+
+  shift() { // removing a node from the beginning of the Doubly Linked List
+    if (!this.head) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
 }
 
 let list = new DoublyLinkedList();
 list.push(0);
 list.push(1);
-console.log(list);
-list.pop();
-console.log(list);
-list.pop();
+// console.log(list);
+// list.pop();
+// console.log(list);
+// list.pop();
+console.log(list.shift());
+list.unshift(-2);
 console.log(list);
 
 
