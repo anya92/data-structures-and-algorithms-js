a5849bf9ec79357c23dc3cefc89b8930c862e2e8
Ania <aniabrajtkopf@gmail.com>
2018-10-23 11:51:43 +0200

    added depth first traversal (recursive and iterative)

--- 7.DataStructures/graphs.js
+++ 7.DataStructures/graphs.js
@@ -32,18 +32,70 @@ class Graph {
       delete this.adjacencyList[vertex];
     }
   }
+
+  depthFirstRecursive(start) {
+    let results = [];
+    let visited = {};
+    const { adjacencyList } = this;
+
+    (function traverse(vertex) {
+      if (!vertex) return;
+      results.push(vertex);
+      visited[vertex] = true;
+      for (let neighbour of adjacencyList[vertex]) {
+        if (!visited[neighbour]) traverse(neighbour);
+      }
+    })(start);
+    return results;
+  }
+
+  depthFirstIterative(start) {
+    let stack = [];
+    let results = [];
+    let visited = {};
+    stack.push(start);
+    visited[start] = true;
+    while (stack.length > 0) {
+      let vertex = stack.pop();
+      results.push(vertex);
+      for (let neighbour of this.adjacencyList[vertex]) {
+        if (!visited[neighbour]) {
+          visited[neighbour] = true;
+          stack.push(neighbour);
+        }
+      }
+    }
+    return results;
+  }
+
 }
 
+let g = new Graph();
+g.addVertex('Berlin');
+g.addVertex('Denver');
+g.addVertex('Oslo');
+g.addVertex('Helsinki');
+g.addEdge('Oslo', 'Helsinki');
+g.addEdge('Oslo', 'Denver');
+g.addEdge('Oslo', 'Berlin');
+g.addEdge('Denver', 'Berlin');
+g.removeEdge('Berlin', 'Oslo');
+console.log(g);
+g.removeVertex('Oslo');
+
 let graph = new Graph();
-graph.addVertex('Berlin');
-graph.addVertex('Denver');
-graph.addVertex('Oslo');
-graph.addVertex('Helsinki');
-graph.addEdge('Oslo', 'Helsinki');
-graph.addEdge('Oslo', 'Denver');
-graph.addEdge('Oslo', 'Berlin');
-graph.addEdge('Denver', 'Berlin');
-graph.removeEdge('Berlin', 'Oslo');
-console.log(graph);
-graph.removeVertex('Oslo');
-console.log(graph);
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "E");
+graph.addEdge("D", "E");
+graph.addEdge("D", "F");
+graph.addEdge("E", "F");
+console.log(graph.depthFirstRecursive('C'));
+console.log(graph.depthFirstIterative('C'));
